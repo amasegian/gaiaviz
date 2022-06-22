c1684234492f00c895acd8f8e120228dbc510494
Alexandra Masegian <alexandramasegian@gmail.com>
2022-06-22 13:58:50 -0700

    Created SkyPatch class with ability to query Gaia.

---
+++ SkyPatch.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from astroquery.gaia import Gaia
+
+class SkyPatch():
+
+    def __init__(self, ra, dec, radius=1, num_sources=None):
+        self.ra = ra
+        self.dec = dec
+        self.radius = radius
+        self.num_sources = num_sources
+
+        query = f"""source_id, ra, dec, pmra, pmdec, parallax, radial_velocity, phot_g_mean_mag
+        FROM gaiadr3.gaia_source
+        WHERE 1=CONTAINS(POINT(ra,dec), CIRCLE({ra},{dec},{radius}))
+        AND radial_velocity IS NOT NULL AND ra IS NOT NULL AND dec IS NOT NULL AND parallax >= 0"""
+
+        if num_sources is not None:
+            query = f"""TOP {num_sources} """ + query
+        query = """SELECT """ + query
+
+        gaia_job = Gaia.launch_job(query)
+        self.data = gaia_job.get_results()
+        self.pandas_data = self.data.to_pandas()
