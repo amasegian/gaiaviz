742ad7591ab0a4cb58b50e3ad2261318d7d8d9d1
Alexandra Masegian <alexandramasegian@gmail.com>
2022-06-25 09:40:34 -0700

    Added more info to setup.py.

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name="gaiaviz",
     version="0.1.1",
-    packages=find_packages()
+    packages=find_packages(),
+    description='Tools for visualizing data for Gaia DR3. Created as part of Code/Astro 2022 to learn about Python package development.',
+    url='https://github.com/amasegian/gaiaviz',
+    keywords='Gaia'
 )
